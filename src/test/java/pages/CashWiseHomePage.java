97d6d761edce8905ae1336a4832b4d217579b9c3
Us8rNaMe <127808250+Us8rNaMe@users.noreply.github.com>
2023-06-12 20:33:32 -0500

    Adding CashWiseHomePage

---
+++ src/test/java/pages/CashWiseHomePage.java
@@ -0,0 +1,35 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class CashWiseHomePage {
+
+    public CashWiseHomePage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(xpath = "(//button)[1]")
+    public WebElement signInBtn;
+
+    @FindBy(xpath = "(//button)[2]")
+    public WebElement signUpBtn;
+
+    @FindBy(xpath = "(//button)[3]")
+    public WebElement signUpDownBtn;
+
+    @FindBy(xpath = "(//button)[4]")
+    public WebElement contactSendBtn;
+
+    @FindBy(xpath = "(//input)[1]")
+    public WebElement contactNameField;
+
+    @FindBy(xpath = "(//input)[2]")
+    public WebElement contactNumberField;
+
+    @FindBy(xpath = "(//input)[3]")
+    public WebElement contactEmailField;
+
+}
