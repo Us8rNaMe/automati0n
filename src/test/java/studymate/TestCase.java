5f1e21b0c6ee9efc45e669621334e133b1150c85
Us8rNaMe <127808250+Us8rNaMe@users.noreply.github.com>
2023-06-14 21:22:36 -0500

    Add new test case for StudyMate StudyMateTest and something else

--- src/test/java/studymate/TestCase.java
+++ src/test/java/studymate/TestCase.java
@@ -1,13 +1,12 @@
 package studymate;
 
 import com.github.javafaker.Faker;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-import pages.StudyMateLoginPage;
+import pages.StudyMatePage;
 import utilities.ApplicationFlow;
 import utilities.Config;
 import utilities.Driver;
@@ -17,42 +16,42 @@ public class TestCase {
     public void driver(){
         Driver.getDriver().get(Config.getValue("URL"));
     }
+    @AfterTest
+    public void cleanUp(){
+        ApplicationFlow.pause(15000);
+        Driver.quit();
+    }
     @Test
     public void createTeacher(){
         Faker faker = new Faker();
         Actions actions = new Actions(Driver.getDriver());
-        StudyMateLoginPage studyMateLoginPage = new StudyMateLoginPage();
-        studyMateLoginPage.emailField.sendKeys(Config.getValue("studyMateEmail"));
-        studyMateLoginPage.passwordField.sendKeys(Config.getValue("studyMatePassword"));
-        studyMateLoginPage.loginBtn.click();
-        studyMateLoginPage.TeacherBtn.click();
-        studyMateLoginPage.addTeacher.click();
+        StudyMatePage studyMatePage = new StudyMatePage();
+        studyMatePage.emailField.sendKeys(Config.getValue("studyMateEmail"));
+        studyMatePage.passwordField.sendKeys(Config.getValue("studyMatePassword"));
+        studyMatePage.loginBtn.click();
+        studyMatePage.TeacherBtn.click();
+        studyMatePage.addTeacher.click();
         String firstName = faker.name().firstName();
         String lastName = faker.name().lastName();
         String lastAndFirstName = firstName+" "+lastName;
-        studyMateLoginPage.firstTeacherNameField.sendKeys(firstName);
-        studyMateLoginPage.lastTeacherNameField.sendKeys(lastName);
-        studyMateLoginPage.phoneTeacherNumberField.sendKeys("2301007780");
-        studyMateLoginPage.emailTeacherField.sendKeys(faker.internet().emailAddress());
-        studyMateLoginPage.specializationTeacherField.sendKeys(faker.job().position());
+        studyMatePage.firstTeacherNameField.sendKeys(firstName);
+        studyMatePage.lastTeacherNameField.sendKeys(lastName);
+        studyMatePage.phoneTeacherNumberField.sendKeys("2301007780");
+        studyMatePage.emailTeacherField.sendKeys(faker.internet().emailAddress());
+        studyMatePage.specializationTeacherField.sendKeys(faker.job().position());
         ApplicationFlow.pause(500);
-        studyMateLoginPage.choseCourseForTeacher.click();
+        studyMatePage.choseCourseForTeacher.click();
         ApplicationFlow.pause(500);
-        studyMateLoginPage.javaCoreCheckBox.click();
+        studyMatePage.javaCoreCheckBox.click();
         ApplicationFlow.pause(500);
         actions.click().perform();
-        studyMateLoginPage.addBtnForTeacher.click();
+        studyMatePage.addBtnForTeacher.click();
         ApplicationFlow.pause(500);
-        if(studyMateLoginPage.expectedName.equals(lastAndFirstName)){
-            Assert.assertEquals(studyMateLoginPage.expectedName,lastAndFirstName);
+        if(studyMatePage.expectedName.equals(lastAndFirstName)){
+            Assert.assertEquals(studyMatePage.expectedName,lastAndFirstName);
         }else {
-            actions.moveToElement(studyMateLoginPage.nextTeacherPageBtn).perform();
+            actions.moveToElement(studyMatePage.nextTeacherPageBtn).perform();
             actions.click().perform();
         }
-
-
-
-
     }
-
 }
